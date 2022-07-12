5e94fa5a434155b75e04c76d7057c7ec97237ab7
Zachery Crandall <zachcran@gmail.com>
2022-07-12 12:25:22 -0500

    Update goldstein-price documentation

--- src/benchmarking/functions/goldstein_price.py
+++ src/benchmarking/functions/goldstein_price.py
@@ -1,12 +1,21 @@
-def goldstein_price(x_1, x_2):
+def goldstein_price(x_1: float, x_2: float) -> float:
     """Goldstein-Price optimization test function.
 
     Goldstein-Price function from https://www.sfu.ca/~ssurjano/goldpr.html.
 
     Function in LaTeX format:
     f(x) = [1 + (x_1 + x_2 + 1)^2 (19 - 14 x_1 + 3 x_1^2 - 14 x_2 + 6 x_1 x_2 + 3 x_2^2)]
-           \times [30 + (2 x_1 - 3 x_2)^2 (18 - 32 x_1 + 12 x_1^2 + 48 x_2 - 36 x_1 x_2 + 27 x_2^2)]"""
-    
+           \times [30 + (2 x_1 - 3 x_2)^2 (18 - 32 x_1 + 12 x_1^2 + 48 x_2 - 36 x_1 x_2 + 27 x_2^2)]
+
+    :param x_1: First parameter, 'x_1'
+    :type x_1: float
+    :param x_2: Second paramter, 'x-2'
+    :type x_2: float
+
+    :return: Result of calculation
+    :rtype: float
+    """
+
     term_1 = (x_1 + x_2 + 1)**2
     term_1 *= 19 - 14 * x_1 + 3 * x_1**2 - 14 * x_2 + 6 * x_1 * x_2 + 3 * x_2**2
     term_1 += 1
@@ -16,6 +25,7 @@ def goldstein_price(x_1, x_2):
     term_2 += 30
 
     return term_1 * term_2
+
 
 def goldstein_price_min() -> float:
     """Global minimum for the Goldstein-Price function.
