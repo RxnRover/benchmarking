403562ef63f8a2a08ae8bc134d4da1de17c3f288
Zachery Crandall <zachcran@gmail.com>
2022-07-12 12:04:23 -0500

    Added min and tests for Shekel function. m = 7, 10 are failing due to precision atm

--- src/benchmarking/functions/shekel.py
+++ src/benchmarking/functions/shekel.py
@@ -1,12 +1,18 @@
+from typing import List
+
 import numpy as np
 
-def shekel(xs, m=10,
-           C=np.array([[ 4.0, 1.0, 8.0, 6.0, 3.0, 2.0, 5.0, 8.0, 6.0, 7.0 ],
-                       [ 4.0, 1.0, 8.0, 6.0, 7.0, 9.0, 3.0, 1.0, 2.0, 3.6 ],
-                       [ 4.0, 1.0, 8.0, 6.0, 3.0, 2.0, 5.0, 8.0, 6.0, 7.0 ],
-                       [ 4.0, 1.0, 8.0, 6.0, 7.0, 9.0, 3.0, 1.0, 2.0, 3.6 ]]),
-           beta=[0.1, 0.2, 0.2, 0.4, 0.4, 0.6, 0.3, 0.7, 0.5, 0.5]
-):
+
+def shekel(xs: List[float],
+           m: int = 10,
+           C: np.ndarray = np.array(
+               [[4.0, 1.0, 8.0, 6.0, 3.0, 2.0, 5.0, 8.0, 6.0, 7.0],
+                [4.0, 1.0, 8.0, 6.0, 7.0, 9.0, 3.0, 1.0, 2.0, 3.6],
+                [4.0, 1.0, 8.0, 6.0, 3.0, 2.0, 5.0, 8.0, 6.0, 7.0],
+                [4.0, 1.0, 8.0, 6.0, 7.0, 9.0, 3.0, 1.0, 2.0, 3.6]]),
+           beta: List[float] = [
+               0.1, 0.2, 0.2, 0.4, 0.4, 0.6, 0.3, 0.7, 0.5, 0.5
+           ]):
     """Sheckel 4D optimization test function.
 
     Sheckel 4D function with (by default) values of \beta and C
@@ -23,8 +29,35 @@ def shekel(xs, m=10,
         inner_sum = 0
 
         for j in range(4):
-            inner_sum += (xs[j] - C[j][i])**2 + beta[i]
+            inner_sum += (xs[j] - C[j][i])**2
 
-        outer_sum += 1 / inner_sum
+        outer_sum += 1 / (inner_sum + beta[i])
 
     return -outer_sum
+
+
+def shekel_min(m: int = 10) -> float:
+    """Shekel function global minimum. This function only supports 'm' values
+    of 5, 7, or 10.
+
+    Global minimum at
+
+    * m = 5 is -10.1532 for (4, 4, 4, 4)
+    * m = 7 is -10.4029 for (4, 4, 4, 4)
+    * m = 10 is -10.5364 for (4, 4, 4, 4)
+
+    :param m: _description_, defaults to 10
+    :type m: int, optional
+    :raises ValueError: _description_
+    :return: _description_
+    :rtype: float
+    """
+
+    if (m == 5):
+        return -10.1532
+    elif (m == 7):
+        return -10.4029
+    elif (m == 10):
+        return -10.5364
+    else:
+        raise ValueError("Invalid 'm' value.")
