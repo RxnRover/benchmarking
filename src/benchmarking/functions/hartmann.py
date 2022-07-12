18bce970e11f5b3830dc85e3b969996c540abe2f
Zachery Crandall <zachcran@gmail.com>
2022-07-12 11:48:46 -0500

    Add min and test for Hartmann function

--- src/benchmarking/functions/hartmann.py
+++ src/benchmarking/functions/hartmann.py
@@ -81,11 +81,17 @@ def hartmann_min(dimensions: int = 3) -> float:
     """Hartmann function global minimum. This function supports 3 or 6
     dimensions.
 
+    In 3 dimensions, the global minimum is -3.86278 at 
+    (0.114614, 0.555649, 0.852547).
+
+    In 6 dimensions, the global minimum is -3.32237 at
+    (0.20169, 0.150011, 0.476874, 0.275332, 0.311652, 0.6573).
+
     :param dimensions: Number of dimensions, defaults to 3
     :type dimensions: int, optional
 
     :raises ValueError: Invalid number of dimensions.
-    
+
     :return: Global minimum.
     :rtype: float
     """
---
+++ tests/benchmarking/test_hartmann.py
@@ -0,0 +1,20 @@
+import unittest
+
+from benchmarking.functions import hartmann
+
+
+class TestHartmann(unittest.TestCase):
+
+    def test_3d(self):
+
+        xs = [0.114614, 0.555649, 0.852547]
+        
+        self.assertAlmostEqual(hartmann.hartmann(xs),
+                               hartmann.hartmann_min(), 5)
+
+    def test_6d(self):
+
+        xs = [0.20169, 0.150011, 0.476874, 0.275332, 0.311652, 0.6573]
+        
+        self.assertAlmostEqual(hartmann.hartmann(xs),
+                               hartmann.hartmann_min(dimensions=6), 5)
