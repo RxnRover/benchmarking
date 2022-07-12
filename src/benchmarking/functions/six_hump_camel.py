9f6daf9e0fce185fe88f5e63bed4c32f5228e60e
Zachery Crandall <zachcran@gmail.com>
2022-07-12 12:37:38 -0500

    Updated six hump camel documentation

--- src/benchmarking/functions/six_hump_camel.py
+++ src/benchmarking/functions/six_hump_camel.py
@@ -1,10 +1,19 @@
-def six_hump_camel(x_1, x_2):
+def six_hump_camel(x_1: float, x_2: float) -> float:
     """Six-hump camel optimization test function.
 
     Six-hump camel function from https://www.sfu.ca/~ssurjano/camel6.html.
 
     Function in LaTeX format:
-    f(x) = (4 - 2.1 x_1^2 + \frac{x_1^4}{3}) x_1^2 + x_1 x_2 + (-4 + 4 x_2^2) x_2^2"""
+    f(x) = (4 - 2.1 x_1^2 + \frac{x_1^4}{3}) x_1^2 + x_1 x_2 + (-4 + 4 x_2^2) x_2^2
+
+    :param x_1: First parameter, 'x_1'
+    :type x_1: float
+    :param x_2: Second parameter, 'x_2'
+    :type x_2: float
+
+    :return: Result of calculation
+    :rtype: float
+    """
 
     term_1 = (4 - 2.1 * x_1**2 + x_1**4 / 3) * x_1**2
     term_2 = x_1 * x_2
