e3872fb239cfb668327a0a7a186b612771d1adb2
zachcran <zachcran@gmail.com>
2022-07-21 11:17:18 -0500

    Added documentation explaining test values

--- tests/benchmarking/test_shubert.py
+++ tests/benchmarking/test_shubert.py
@@ -7,6 +7,10 @@ class TestShubert(unittest.TestCase):
 
     def test_single_min(self):
 
+        # These input values were found manually. Since this function is
+        # periodic, there should be infinite global minima. Ideally,
+        # an analysis of extrema should be performed and multiple minima
+        # based on that analysis should be tested.
         x_1 = -1.4251
         x_2 = -0.8004
         
