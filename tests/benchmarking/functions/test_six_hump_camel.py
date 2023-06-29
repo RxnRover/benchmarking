a760a36d3d4502285a0a7a65559c307f7b36431b
zachcran <zachcran@gmail.com>
2023-06-29 12:52:09 -0500

    Fixed test name for six hump camel test

--- tests/benchmarking/functions/test_six_hump_camel.py
+++ tests/benchmarking/functions/test_six_hump_camel.py
@@ -3,7 +3,7 @@ import unittest
 from benchmarking.functions import six_hump_camel
 
 
-class TestBranin(unittest.TestCase):
+class TestSixHumpCamel(unittest.TestCase):
     def test_min_1(self):
         x = [0.0898, -0.7126]
 
