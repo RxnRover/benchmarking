2649a6806b4c33469e6a35c4d1cf89966f289df3
Zachery Crandall <zachcran@gmail.com>
2023-08-30 11:58:45 -0500

    Remove some remaining debugging output messages in the Hartmann function implementation (#9)

--- src/benchmarking/functions/hartmann.py
+++ src/benchmarking/functions/hartmann.py
@@ -6,10 +6,12 @@ import numpy as np
 supported_dimensions = [3, 6]
 
 
-def hartmann(xs: List[float],
-             alpha: np.ndarray = None,
-             A: np.ndarray = None,
-             P: np.ndarray = None) -> float:
+def hartmann(
+    xs: List[float],
+    alpha: np.ndarray = None,
+    A: np.ndarray = None,
+    P: np.ndarray = None,
+) -> float:
     """Hartmann n-Dimensional optimization test function. This function
     supports 3 or 6 dimensions, and dimensions are inferred by the length
     of the ``xs`` parameter list.
@@ -18,7 +20,8 @@ def hartmann(xs: List[float],
     from https://www.sfu.ca/~ssurjano/hart3.html.
 
     Function in LaTeX format:
-    f(x) = -\sum_{i=1}^{4} \alpha_i \exp{\bigg(-\sum_{j=1}^n A_{ij}(x_j - P_{ij})^2\bigg)}
+    f(x) = -\sum_{i=1}^{4} \alpha_i
+           \exp{\bigg(-\sum_{j=1}^n A_{ij}(x_j - P_{ij})^2\bigg)}
 
     :param xs: Input parameters
     :type xs: List[float]
@@ -42,40 +45,50 @@ def hartmann(xs: List[float],
     dimension_count = len(xs)
 
     # Check dimension count
-    if (dimension_count not in supported_dimensions):
+    if dimension_count not in supported_dimensions:
         raise ValueError("Invalid dimension count.")
 
     # Check for provided alpha value
-    if (alpha == None):
-        print("No alpha detected. Using default.")
-
-        if (dimension_count == 3 or dimension_count == 6):
+    if alpha is None:
+        if dimension_count == 3 or dimension_count == 6:
             alpha = np.array([1.0, 1.2, 3.0, 3.2])
 
     # Check for provided A values
-    if (A == None):
-        print("No A values detected. Using default.")
-
-        if (dimension_count == 3):
-            A = np.array([[3.0, 10, 30], [0.1, 10, 35], [3.0, 10, 30],
-                          [0.1, 10, 35]])
-        elif (dimension_count == 6):
-            A = np.array([[10, 3, 17, 3.50, 1.7, 8],
-                          [0.05, 10, 17, 0.1, 8, 14], [3, 3.5, 1.7, 10, 17, 8],
-                          [17, 8, 0.05, 10, 0.1, 14]])
+    if A is None:
+        if dimension_count == 3:
+            A = np.array(
+                [[3.0, 10, 30], [0.1, 10, 35], [3.0, 10, 30], [0.1, 10, 35]]
+            )
+        elif dimension_count == 6:
+            A = np.array(
+                [
+                    [10, 3, 17, 3.50, 1.7, 8],
+                    [0.05, 10, 17, 0.1, 8, 14],
+                    [3, 3.5, 1.7, 10, 17, 8],
+                    [17, 8, 0.05, 10, 0.1, 14],
+                ]
+            )
 
     # Check for provided P values
-    if (P == None):
-        print("No P values detected. Using default.")
-
-        if (dimension_count == 3):
-            P = 10**(-4) * np.array([[3689, 1170, 2673], [4699, 4387, 7470],
-                                     [1091, 8732, 5547], [381, 5743, 8828]])
-        elif (dimension_count == 6):
-            P = 10**(-4) * np.array([[1312, 1696, 5569, 124, 8283, 5886],
-                                     [2329, 4135, 8307, 3736, 1004, 9991],
-                                     [2348, 1451, 3522, 2883, 3047, 6650],
-                                     [4047, 8828, 8732, 5743, 1091, 381]])
+    if P is None:
+        if dimension_count == 3:
+            P = 10 ** (-4) * np.array(
+                [
+                    [3689, 1170, 2673],
+                    [4699, 4387, 7470],
+                    [1091, 8732, 5547],
+                    [381, 5743, 8828],
+                ]
+            )
+        elif dimension_count == 6:
+            P = 10 ** (-4) * np.array(
+                [
+                    [1312, 1696, 5569, 124, 8283, 5886],
+                    [2329, 4135, 8307, 3736, 1004, 9991],
+                    [2348, 1451, 3522, 2883, 3047, 6650],
+                    [4047, 8828, 8732, 5743, 1091, 381],
+                ]
+            )
 
     # Start the outer sum at zero
     outer_sum = 0
@@ -98,7 +111,7 @@ def hartmann_min(dimensions: int = 3) -> float:
     """Hartmann function global minimum. This function supports 3 or 6
     dimensions.
 
-    In 3 dimensions, the global minimum is -3.86278 at 
+    In 3 dimensions, the global minimum is -3.86278 at
     (0.114614, 0.555649, 0.852547).
 
     In 6 dimensions, the global minimum is -3.32237 at
@@ -113,9 +126,9 @@ def hartmann_min(dimensions: int = 3) -> float:
     :rtype: float
     """
 
-    if (dimensions == 3):
+    if dimensions == 3:
         return -3.86278
-    elif (dimensions == 6):
+    elif dimensions == 6:
         return -3.32237
     else:
         raise ValueError("Invalid dimension count.")
